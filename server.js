10aa5515e362914913061f1962f52fe6c9af9312
rende99 <rende99@gmail.com>
2020-08-02 12:08:41 -0400

    new build

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ server.listen(port, () => console.log(`Listening on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
 }
 
 
