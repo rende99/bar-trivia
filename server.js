d625c6bd603d918dfd233320d3f47e660a9eb2fa
rende99 <rende99@gmail.com>
2020-08-02 12:54:21 -0400

    changed server routing

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ server.listen(port, () => console.log(`Listening on port ${port}`));
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'build')))
+    MongoClient.connect(process.env.mongo_url, { useUnifiedTopology: true })
+    .then(client => {
+        app.get('/*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
+    })
 }
 
 
